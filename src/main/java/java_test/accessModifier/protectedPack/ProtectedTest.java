caf9bb7461a226bad28a1348d55b7e2e4a86940b
jakyoungkim <jakyoung.kim@catenoid.net>
2020-05-19 14:16:55 +0900

    protected

--- src/main/java/java_test/accessModifier/protectedPack/ProtectedTest.java
+++ src/main/java/java_test/accessModifier/protectedPack/ProtectedTest.java
@@ -2,11 +2,18 @@ package java_test.accessModifier.protectedPack;
 
 import java_test.accessModifier.TestClass;
 
-public class ProtectedTest {
+public class ProtectedTest extends TestClass {
+
+	public ProtectedTest(Integer value) {
+		super(value);
+	}
 
 	public static void main(String[] args) {
 		TestClass tc = new TestClass(200);
+		System.out.println(tc.pValue);
 		System.out.println(tc.proValue);
+		ProtectedTest pt = new ProtectedTest(300);
+		System.out.println(pt.proValue);
 	}
 
 }
